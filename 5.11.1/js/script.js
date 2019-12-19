78a276d10af5fda87a94f47bbec71286fe3dd79d
LighterEugene <51925892+LighterEugene@users.noreply.github.com>
2019-12-19 14:01:41 +0300

    Update script.js

--- 5.11.1/js/script.js
+++ 5.11.1/js/script.js
@@ -8,7 +8,11 @@ let names = [
 ['обезьяна', 'рысь']
 ]
 ];
-for(name of names){
-	console.log(name);
-}
-
+for (name1 of names [0][0] ){
+ console.log(name1)}
+for (name2 of names [0][1] ){
+ console.log(name2)}
+ for (name3 of names [1][0] ){
+ console.log(name3)}
+ for (name4 of names [1][1] ){
+ console.log(name4)}
